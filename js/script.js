41b40278e095cfe991a15a43f448d34bae7aa517
Piotr Rozanski <piter05@o2.pl>
2022-10-29 12:06:52 +0200

    add function in JS

--- js/script.js
+++ js/script.js
@@ -1,37 +1,42 @@
-let buttonElement = document.querySelector(".js-button");
-let formElement = document.querySelector(".js-form");
-let sumElement = document.querySelector(".js-sum");
-let inputElement = document.querySelector(".js-inputCurrency");
-let outputElement = document.querySelector(".js-outputCurrency");
-let resultElement = document.querySelector(".js-result");
+{
+    const buttonElement = document.querySelector(".js-button");
+    const formElement = document.querySelector(".js-form");
+    const sumElement = document.querySelector(".js-sum");
+    const inputElement = document.querySelector(".js-inputCurrency");
+    const outputElement = document.querySelector(".js-outputCurrency");
+    const resultElement = document.querySelector(".js-result");
+    
+    const caculatedResult = () => {
+        const PlnToEur = 0.18476;
+        const PlnToUsd = 0.21453;
+        const UsdToEur = 0.99190;
 
-let PlnToEur = 0.18476;
-let PlnToUsd = 0.21453;
-let UsdToEur = 0.99190;
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+            const inputCurrency = inputElement.value;
+            const outputCurrency = outputElement.value;
+            const sum = sumElement.value;
 
-    let inputCurrency = inputElement.value;
-    let outputCurrency = outputElement.value;
-    let sum = sumElement.value;
-
-    switch (inputCurrency) {
-        case "PLN":
-            if (outputCurrency === "PLN") result = (sum * 1);
-            else if (outputCurrency === "EUR") result = (sum * PlnToEur);
-            else if (outputCurrency === "USD") result = (sum * PlnToUsd);
-            break;
-        case "USD":
-            if (outputCurrency === "USD") result = (sum * 1);
-            else if (outputCurrency === "EUR") result = (sum * UsdToEur);
-            else if (outputCurrency === "PLN") result = (sum / PlnToUsd);
-            break;
-        case "EUR":
-            if (outputCurrency === "EUR") result = (sum * 1);
-            else if (outputCurrency === "PLN") result = (sum / PlnToEur);
-            else if (outputCurrency === "USD") result = (sum / UsdToEur);
-            break;
+            switch (inputCurrency) {
+                case "PLN":
+                    if (outputCurrency === "PLN") result = (sum * 1);
+                    else if (outputCurrency === "EUR") result = (sum * PlnToEur);
+                    else if (outputCurrency === "USD") result = (sum * PlnToUsd);
+                    break;
+                case "USD":
+                    if (outputCurrency === "USD") result = (sum * 1);
+                    else if (outputCurrency === "EUR") result = (sum * UsdToEur);
+                    else if (outputCurrency === "PLN") result = (sum / PlnToUsd);
+                    break;
+                case "EUR":
+                    if (outputCurrency === "EUR") result = (sum * 1);
+                    else if (outputCurrency === "PLN") result = (sum / PlnToEur);
+                    else if (outputCurrency === "USD") result = (sum / UsdToEur);
+                    break;
+            }
+            resultElement.innerText = `${result.toFixed(2)} ${outputCurrency}`;
+        });
     }
-    resultElement.innerText = `${result.toFixed(2)} ${outputCurrency}`;
-});
+    caculatedResult()
+}
