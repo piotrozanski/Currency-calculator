14d609fb707582c9e161be79b7ba3075a91aab88
Piotr Rozanski <piter05@o2.pl>
2022-11-07 19:56:05 +0100

    change js script, add new background images

--- js/script.js
+++ js/script.js
@@ -1,42 +1,75 @@
 {
-    const buttonElement = document.querySelector(".js-button");
-    const formElement = document.querySelector(".js-form");
-    const sumElement = document.querySelector(".js-sum");
-    const inputElement = document.querySelector(".js-inputCurrency");
-    const outputElement = document.querySelector(".js-outputCurrency");
-    const resultElement = document.querySelector(".js-result");
-    
-    const caculatedResult = () => {
-        const PlnToEur = 0.18476;
-        const PlnToUsd = 0.21453;
-        const UsdToEur = 0.99190;
-
-        formElement.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const inputCurrency = inputElement.value;
-            const outputCurrency = outputElement.value;
-            const sum = sumElement.value;
-
-            switch (inputCurrency) {
-                case "PLN":
-                    if (outputCurrency === "PLN") result = (sum * 1);
-                    else if (outputCurrency === "EUR") result = (sum * PlnToEur);
-                    else if (outputCurrency === "USD") result = (sum * PlnToUsd);
-                    break;
-                case "USD":
-                    if (outputCurrency === "USD") result = (sum * 1);
-                    else if (outputCurrency === "EUR") result = (sum * UsdToEur);
-                    else if (outputCurrency === "PLN") result = (sum / PlnToUsd);
-                    break;
-                case "EUR":
-                    if (outputCurrency === "EUR") result = (sum * 1);
-                    else if (outputCurrency === "PLN") result = (sum / PlnToEur);
-                    else if (outputCurrency === "USD") result = (sum / UsdToEur);
-                    break;
-            }
-            resultElement.innerText = `${result.toFixed(2)} ${outputCurrency}`;
-        });
+    const exchangeUSD = (value, outputCurrency) => {
+        switch (outputCurrency) {
+            case "USD":
+                return value;
+            case "PLN":
+                return value * 5.53;
+            case "EUR":
+                return value * 1.03;
+        }
     }
-    caculatedResult()
-}
+
+    const exchangePLN = (value, outputCurrency) => {
+        const usd = 4.89;
+        const euro = 4.84;
+
+
+        switch (outputCurrency) {
+            case "PLN":
+                return value;
+            case "USD":
+                return value / usd;
+            case "EUR":
+                return value / euro;
+        }
+    }
+
+    const exchangeEUR = (value, outputCurrency) => {
+        switch (outputCurrency) {
+            case "EUR":
+                return value;
+            case "USD":
+                return value * 0.9694;
+            case "PLN":
+                return value * 4.84;
+        }
+    }
+
+        const currencyExchange = (value, inputcurrency, outputCurrency) => {
+
+            switch (inputcurrency) {
+                case "USD":
+                    return exchangeUSD(value, outputCurrency);
+                case "PLN":
+                    return exchangePLN(value, outputCurrency);
+                case "EUR":
+                    return exchangeEUR(value, outputCurrency);
+            }
+        };
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const valueElement = document.querySelector(".js-value");
+        const resultElement = document.querySelector(".js-result");
+        const inputElement = document.querySelector(".js-inputCurrency");
+        const outputElement = document.querySelector(".js-outputCurrency");
+
+        const value = +valueElement.value;
+        const inputCurrency = inputElement.value;
+        const outputCurrency = outputElement.value;
+        const result = currencyExchange(value, inputCurrency, outputCurrency);
+
+        resultElement.innerHTML = `${result.toFixed(2)} ${outputCurrency}`;
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+};
